714297a2bbceba8750b735ff7aef8562b03fcb84
Vânia Almeida <vanialadev@gmail.com>
2017-08-16 00:58:31 -0300

    criando a primieria app e configurando a home

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q_i!jc5_v=kq)ys)9#2yi*cclo_!rkn2mk06bj)&76tjtj-$xu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '192.168.1.5']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'simplemooc.core',
 ]
 
 MIDDLEWARE = [
--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from simplemooc.core import views
 
 urlpatterns = [
+    # url(r'^$', include('simplemooc.core.urls')),
+    url(r'^$', views.home, name='home'),
+    url(r'^contato/$', views.contato, name='contato'),
     url(r'^admin/', admin.site.urls),
+
 ]
