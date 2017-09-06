4cb0b3b9fe0246aeb0673e49858bc4bd04b24328
Vânia Almeida <vanialadev@gmail.com>
2017-09-06 03:27:36 -0300

    página da dashboard do usuário

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout
-from simplemooc.accounts.views import register
+from simplemooc.accounts.views import register, dashboard
 
 urlpatterns = [
+    url(r'^$', dashboard, name='dashboard'),
     url(r'^entrar/$', login,
         {'template_name' : 'accounts/login.html'}, name='login'),
     url(r'^sair/$', logout,
