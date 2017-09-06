46c0d7d5d9f7767ea53c6dd3dcff737f570bc9aa
Vânia Almeida <vanialadev@gmail.com>
2017-09-06 02:29:11 -0300

    logout

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
-from django.template.backends import django
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 from simplemooc.accounts.views import register
 
 urlpatterns = [
     url(r'^entrar/$', login,
-        {'template_name' : 'accounts/login.html'},
-        name='login'),
-url(r'^cadastre-se/$', register, name='register'),
+        {'template_name' : 'accounts/login.html'}, name='login'),
+    url(r'^sair/$', logout,
+        {'next_page' : 'core:home'}, name='logout'),
+    url(r'^cadastre-se/$', register, name='register'),
 
 ]
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,6 +1,5 @@
-from django.conf import settings
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from .forms import RegisterForm
 
 
@@ -9,8 +8,12 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect(settings.LOGIN_URL)
+            user = form.save()
+            user = authenticate(
+                username=user.username, password=form.cleaned_data['password1']
+            )
+            login(request, user)
+            return redirect('core:home')
     else:
         form = RegisterForm()
     context = {
