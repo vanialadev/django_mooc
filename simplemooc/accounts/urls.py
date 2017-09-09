36c098c893937e0824e5c1ecf9ffad7691404137
Vânia Almeida <vanialadev@gmail.com>
2017-09-09 07:01:17 -0300

    criando o template, view, form,url da dashboard e do editar a conta do usuário

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout
-from simplemooc.accounts.views import register, dashboard
+from simplemooc.accounts.views import register, dashboard, edit
 
 urlpatterns = [
     url(r'^$', dashboard, name='dashboard'),
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^sair/$', logout,
         {'next_page' : 'core:home'}, name='logout'),
     url(r'^cadastre-se/$', register, name='register'),
-
+    url(r'^editar/$', edit, name='edit'),
 ]
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import RegisterForm
+from .forms import RegisterForm, EditAccountForm
 
 
 @login_required
@@ -25,4 +25,19 @@ def register(request):
     context = {
         'form' : form
     }
+    return render(request, template_name, context)
+
+@login_required
+def edit(request):
+    template_name= 'accounts/edit.html'
+    context = {}
+    if request.method == 'POST':
+        form =EditAccountForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            form = EditAccountForm(instance=request.user)
+            context['success'] = True
+    else:
+        form = EditAccountForm(instance=request.user)
+    context['form'] = form
     return render(request, template_name, context)
